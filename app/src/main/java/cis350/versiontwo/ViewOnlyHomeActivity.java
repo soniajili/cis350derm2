3a3bdcc568bd1a4d35ae7b8ebd186eff66387b42
Sonia <Sonia@seas0954.wireless-pennnet.upenn.edu>
2015-04-01 16:40:44 -0400

    anna's new home page

--- app/src/main/java/cis350/versiontwo/ViewOnlyHomeActivity.java
+++ app/src/main/java/cis350/versiontwo/ViewOnlyHomeActivity.java
@@ -1,17 +1,50 @@
 package cis350.versiontwo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ViewOnlyHomeActivity extends ActionBarActivity {
+
+    Button viewCollectionButton;
+    Button searchButton;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_only_home);
+        viewCollectionButton = (Button) findViewById(R.id.viewCollectionButton2);
+        viewCollectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CollectionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        searchButton = (Button) findViewById(R.id.searchButton2);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logoutButton = (Button) findViewById(R.id.logoutButton2);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
