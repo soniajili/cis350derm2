08bfa87fe80a25431ebaaff23e209fcea077b5b0
Sonia <Sonia@levyvlan502.3587.wlan.wireless-resnet.upenn.edu>
2015-04-13 22:21:20 -0400

    THE NEW AND IMPROVED COLLECTION

--- app/src/main/java/cis350/versiontwo/CollectionActivity.java
+++ app/src/main/java/cis350/versiontwo/CollectionActivity.java
@@ -1,11 +1,23 @@
 package cis350.versiontwo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class CollectionActivity extends ActionBarActivity {
@@ -15,85 +27,54 @@ public class CollectionActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection);
 
+        //Enable Parse
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "fviaFJ9B1jQdWCCnS419jkZ8dFVquHBd1lu0Y1jF",
+                "p6dYSbB0KVF7KPvstO2ui7B32RanUEj9vmS28DLi");
+
+
         ParseQuery<ParseObject> query = ParseQuery.getQuery
                 ("collectionImages");
         query.whereEqualTo("objectType", "image");
-        /*
+
         query.findInBackground(new FindCallback<ParseObject>() {
+
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
 
                 if (e == null) {
-                    ImageView image = (ImageView) findViewById(R.id.image);
+                    for (int i = 0; i < objects.size(); i++) {
+                        ParseObject object = objects.get(i);
+                        ParseFile fileObject = (ParseFile) object.get
+                                ("file");
+                        Toast.makeText(getApplicationContext(),
+                                "getting fileObjects",
+                                Toast.LENGTH_SHORT).show();
+                        fileObject.getDataInBackground(new GetDataCallback() {
+                            public void done(byte[] data, ParseException e) {
+                                if (e == null) {
+                                    ImageView imageView = (ImageView) findViewById(R.id.image);
+                                    Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                    imageView.setImageBitmap(bmp);
 
-                } else {
+                                } else {
+                                    Log.d("test",
+                                            "There was a problem downloading the data.");
+                                }
+                            }
+                        });
+                    }
+                }
+
+                else{
                     Toast.makeText(getApplicationContext(),
                             "error retrieving images",
                             Toast.LENGTH_SHORT).show();
-                } */
-
-                /*
-                for(int i =0 ; i < objects.size(); i++){
-
-                    ParseObject object = objects.get(i);
-                    ParseFile fileObject = (ParseFile) object.get("imageFile");
-                    fileObject.getDataInBackground(new GetDataCallback() {
-                        public void done(byte[] data,ParseException e) {
-                            if (e == null) {
-
-                                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
-                                imgs[i].setImageBitmap(bmp);
-
-                            } else {
-                                Log.d("test",
-                                        "There was a problem downloading the data.");
-                            }
-                        }
-                    });
                 }
-                progressDialog.dismiss();
+
+
             }
         });
-
-        ParseQuery query = new ParseQuery("imageCollection");
-        query.getInBackground("objectType", new GetCallback() {
-            @Override
-            public void done(ParseObject object, ParseException e) {
-                if (e == null) {
-                    Log.d("test", "The object was not found...");
-                    ParseFile fileObject = (ParseFile)object.get("images");
-                    ParseImageView imageView = (ParseImageView) findViewById(android.R.id.icon);
-                    // The placeholder will be used before and during the fetch, to be replaced by the fetched image
-                    // data.
-                    imageView.setPlaceholder(getResources().getDrawable(R.drawable.placeholder));
-                    imageView.setParseFile(file);
-                    imageView.loadInBackground(new GetDataCallback() {
-                        @Override
-                        public void done(byte[] data, ParseException e) {
-                            Log.i("ParseImageView",
-                                    "Fetched! Data length: " + data.length + ", or exception: " + e.getMessage());
-                        }
-                    });
-                    /*
-                    fileObject.getDataInBackground(new GetDataCallback() {
-                        public void done(byte[] data, ParseException e) {
-                            if (e == null) {
-                                Log.d("test", "We've got data in data.");
-                                // use data for something
-
-                            } else {
-                                Log.d("test", "There was a problem downloading the data.");
-                            }
-                        }
-                    });
-
-                } else {
-                    Log.d("error", "Error retrieving images");
-
-                }
-            }
-        });
-        /*link: https://www.parse.com/questions/retrieve-image-from-parse */
 
     }
 
